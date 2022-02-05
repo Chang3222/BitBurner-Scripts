851718217a0b8447d9d1be9282ed499938c68fa2
Chang <danielchangsp@gmail.com>
2022-02-05 18:23:17 -0300

    Aprimoramento do script maxhack.js

--- maxhack.js
+++ maxhack.js
@@ -3,6 +3,20 @@ import { getOpenServers, maxDeploy, sudoAllPossible } from "util.js";
 export async function main(ns) {
 	let bob = true;
 	let targets;
+	if (ns.args[0]) {
+		await sudoAllPossible(ns, "home");
+		let target = ns.args[0];
+		targets = getOpenServers(ns, "home");
+		for (let server of targets) {
+			ns.tprint("Deploying function on " + server + '\n');
+			await maxDeploy(ns, server, [target], "hack.js");
+		}
+		while (ns.getServerMoneyAvailable(target) > 0) await ns.asleep(1000);
+		for (let server of targets) {
+			if (server == "home") continue;
+			ns.killall(server);
+		}
+	}
 	while (bob) {
 		bob = false;
 		await sudoAllPossible(ns, "home");
